eb12e0346e50090383191ba05a2d37907af9855c
Connor Li <connorli0@outlook.com>
2023-02-23 17:39:34 -0500

    update

--- coding/dotnetSampleApp/dotnetSampleApp/Program.cs
+++ coding/dotnetSampleApp/dotnetSampleApp/Program.cs
@@ -1,15 +1,14 @@
 ﻿using sampleTriangleUtils;
-
+using sampleCalculator;
 
 String result = "NONE";
-
 Console.WriteLine("hello world");
 
 // init trianleUtil with default values
 TriangleUtil trianleUtil = new TriangleUtil();
 Console.Write("Triangle Type Test :-\n----------------------------\n\n");  
 
-
+// sample tun for triangle classes
 result = trianleUtil.CheckTriangleType(12, 12, 12);
 Console.WriteLine(result);
 
@@ -18,3 +17,10 @@ Console.WriteLine(result);
 
 result = trianleUtil.CheckTriangleType(12, 20, 25);  
 Console.WriteLine(result);
+
+// sample tun for calculator classes
+Console.WriteLine("Calculator Test :-\n----------------------------\n\n");
+Console.WriteLine(Calculator.Add(12, 12));
+Console.WriteLine(Calculator.Subtract(12, 12));
+Console.WriteLine(Calculator.Multiply(12, 12));
+Console.WriteLine(Calculator.Divide(12, 12));
---
+++ coding/dotnetSampleApp/readme.md
@@ -0,0 +1,13 @@
+# What does this library do?
+This library is a sample library that can be used to perform basic dotnet build testing.
+
+## Calculator library
+this library is a calculator library that can be used to perform basic arithmetic operations.
+
+## Triangle library
+this library is a triangle library that can be used to perform basic triangle operations.
+
+Triangle library is based on the calculator library.
+
+## dotnet Sample App
+this is a sample app that uses the calculator library and the triangle library.
